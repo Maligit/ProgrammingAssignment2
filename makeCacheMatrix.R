84f575386cbee5f9195f9a9e3eed9b3b5a5ef0f2
Ron M <vermali@gmail.com>
2014-07-26 13:58:30 -0400

    cachedMatrix
    Complete assignments and added comments

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a list of setters and getters methods that can be used to 
+## retrieve the inverse of a squared matrix.
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix takes a squared matrix and create a lists of sub functions
+## that can be used to set the value of the matrix based on what is being passed,
+## get that matrix and similarly set and get the inverse of that matrix using
+## the setinverse() and getinverse() calls. 
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -12,8 +16,10 @@ makeCacheMatrix <- function(x = matrix()) {
       m <<- NULL
   }
   get = function() x
+  ##  set the inverse of a matrix using the solve function 
+  setinverse <- function(solve) m <<- solve
   
-  setinverse function(solve) m <<- solve
+  ##  getinverse returns the inverse of a matrix
   getinverse <- function() m
   
   list(set = set, get = get,
@@ -21,9 +27,20 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## the cacheSolve is used to get a cached version of a matrix if 
+## it exists or just computes it if not. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if (!is.null(m)){
+        	message("getting cached inverse")
+        	return(m)
+        }
+        matrix <- x$get()
+        m <- solve(matrix, ...)
+        x$setinverse(m)
+        m
 }
+
+
